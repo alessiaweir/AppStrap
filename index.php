84a92bccaa9d88659c209e5d219df1e56508be72
Alessia Weir <alessiaweir@gmail.com>
2017-02-07 11:47:07 +0000

    First Theme only commit (First commit included all WP files..oops)

--- index.php
+++ index.php
@@ -1,17 +1,27 @@
+<?php get_header(); ?>
+
+<?php get_template_part( 'navigation', 'default' ); ?>
+
+<?php get_template_part('pageheader', 'default'); ?>
+
+<div class="container main-container">
+  <div class="row">
+    <div class="col-lg-9">
 <?php
-/**
- * Front to the WordPress application. This file doesn't do anything, but loads
- * wp-blog-header.php which does and tells WordPress to load the theme.
- *
- * @package WordPress
- */
+  if ( have_posts() ) :
+    while ( have_posts() ) : the_post();
+      get_template_part( 'content', get_post_format() );
+    endwhile;
+      appstrap_the_posts_pagination();
+  else :
+    ?><p><?php _e( 'Sorry, no posts matched your criteria.', 'appstrap' ); ?></p><?php
+  endif;
+?>
+    </div><!-- .col -->
+    <div class='col-lg-3'>
+      <?php dynamic_sidebar( 'Right Sidebar' ); ?>
+    </div><!-- .col -->
+  </div><!-- .row -->
+</div><!-- .container.main-container -->
 
-/**
- * Tells WordPress to load the WordPress theme and output it.
- *
- * @var bool
- */
-define('WP_USE_THEMES', true);
-
-/** Loads the WordPress Environment and Template */
-require( dirname( __FILE__ ) . '/wp-blog-header.php' );
+<?php get_footer(); ?>
